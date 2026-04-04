c119069cc47ab0d4bcc3d9ea28ffd4aa6b3b676b
shivanshx7 <shivanshsharma62975@gmail.com>
2026-04-04 23:26:54 +0530

    card addition

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const url1 = `https://api.rawg.io/api/games?key=713715bfecfd475486ed3f7ef8803ade&ordering=-added&page_size=40`
 const carousel = document.getElementsByClassName('carousel-kid')
 const carouselRating = document.getElementsByClassName('carouselRating')
+const carouselName = document.getElementsByClassName('carouselName')
+
 
 async function carouselApp() {
     let fetched = await fetch(url1)
@@ -9,33 +11,110 @@ async function carouselApp() {
 
 
     carousel[0].style.backgroundImage = `url(${fetched.results[0].background_image})`
-    carouselRating[0].textContent = `- Rated ${fetched.results[0].rating} by ${fetched.results[0].ratings_count} gamers`
+    carouselRating[0].textContent = `Rated ${fetched.results[0].rating} by ${fetched.results[0].ratings_count} gamers`
+    carouselName[0].textContent = `${fetched.results[0].name} `
+    for (i of fetched.results[0].genres){
+  
+        carousel[0].innerHTML += `<p style="display: inline-block;background-color: #4E00DF;font-family:'Inter',sans-serif ;margin:0px 5px;padding: 4px ;border-radius:5px;font-size:small;font-weight:bold">${i.name.toUpperCase()}</p>`
+
+    }
+
 
     carousel[1].style.backgroundImage = `url(${fetched.results[1].background_image})`
-    carouselRating[1].textContent = `- Rated ${fetched.results[1].rating} by ${fetched.results[1].ratings_count} gamers`
+    carouselRating[1].textContent = `Rated ${fetched.results[1].rating} by ${fetched.results[1].ratings_count} gamers`
+    carouselName[1].textContent = `${fetched.results[1].name} `
+    for (i of fetched.results[1].genres){
+  
+        carousel[1].innerHTML += `<p style="display: inline-block;background-color: #4E00DF;font-family:'Inter',sans-serif ;margin:0px 5px;padding: 4px ;border-radius:5px;font-size:small;font-weight:bold">${i.name.toUpperCase()}</p>`
+
+    }
+    
+
 
     carousel[2].style.backgroundImage = `url(${fetched.results[2].background_image})`
-    carouselRating[2].textContent = `- Rated ${fetched.results[2].rating} by ${fetched.results[2].ratings_count} gamers`
+    carouselRating[2].textContent = `Rated ${fetched.results[2].rating} by ${fetched.results[2].ratings_count} gamers`
+    carouselName[2].textContent = `${fetched.results[2].name} `
+    for (i of fetched.results[2].genres){
+  
+        carousel[2].innerHTML += `<p style="display: inline-block;background-color: #4E00DF;font-family:'Inter',sans-serif ;margin:0px 5px;padding: 4px ;border-radius:5px;font-size:small;font-weight:bold">${i.name.toUpperCase()}</p>`
+
+    }
+
 
     carousel[3].style.backgroundImage = `url(${fetched.results[3].background_image})`
-    carouselRating[3].textContent = `- Rated ${fetched.results[3].rating} by ${fetched.results[3].ratings_count} gamers`
+    carouselRating[3].textContent = `Rated ${fetched.results[3].rating} by ${fetched.results[3].ratings_count} gamers`
+    carouselName[3].textContent = `${fetched.results[3].name} `
+   for (i of fetched.results[3].genres){
+  
+        carousel[3].innerHTML += `<p style="display: inline-block;background-color: #4E00DF;font-family:'Inter',sans-serif ;margin:0px 5px;padding: 4px ;border-radius:5px;font-size:small;font-weight:bold">${i.name.toUpperCase()}</p>`
+
+    }
+
 
     carousel[4].style.backgroundImage = `url(${fetched.results[4].background_image})`
-    carouselRating[4].textContent = `- Rated ${fetched.results[4].rating} by ${fetched.results[0].ratings_count} gamers`
+    carouselRating[4].textContent = `Rated ${fetched.results[4].rating} by ${fetched.results[0].ratings_count} gamers`
+    carouselName[4].textContent = `${fetched.results[4].name} `
+    for (i of fetched.results[4].genres){
+  
+        carousel[4].innerHTML += `<p style="display: inline-block;background-color: #4E00DF;font-family:'Inter',sans-serif ;margin:0px 5px;padding: 4px ;border-radius:5px;font-size:small;font-weight:bold">${i.name.toUpperCase()}</p>`
+
+    }
+
 
     carousel[5].style.backgroundImage = `url(${fetched.results[5].background_image})`
-    carouselRating[5].textContent = `- Rated ${fetched.results[5].rating} by ${fetched.results[0].ratings_count} gamers`
+    carouselRating[5].textContent = `Rated ${fetched.results[5].rating} by ${fetched.results[0].ratings_count} gamers`
+    carouselName[5].textContent = `${fetched.results[5].name}`
+    for (i of fetched.results[5].genres){
+  
+        carousel[5].innerHTML += `<p style="display: inline-block;background-color: #4E00DF;font-family:'Inter',sans-serif ;margin:0px 5px;padding: 4px ;border-radius:5px;font-size:small;font-weight:bold">${i.name.toUpperCase()}</p>`
+
+    }
 
 
 
 
 
 
-    console.log(carouselRating)
+
 }
 
 carouselApp()
 
+const mostPopular = document.getElementById('most-popular')
+
+async function mostPopularApp() {
+    let fetched = await fetch('https://api.rawg.io/api/games?key=713715bfecfd475486ed3f7ef8803ade&ordering=-added&page_size=9')
+    fetched = await fetched.json()
+
+    for (i of fetched.results){
+        mostPopular.innerHTML += `<div><h3>${i.name}</h3><img src=${i.background_image} ></div>`
+    }
+
+
+    console.log(fetched.results)
+
+    
+}
+mostPopularApp()
+
+const PCgames = document.getElementById('pc-games')
+
+async function PCgamesApp() {
+    let fetched = await fetch('https://api.rawg.io/api/games?key=713715bfecfd475486ed3f7ef8803ade&platforms=186')
+    fetched = await fetched.json()
+
+    for (i of fetched.results){
+        PCgames.innerHTML += `<div><h3>${i.name}</h3><img src=${i.background_image} ></div>`
+    }
+
+
+    console.log(fetched.results)
+
+    
+
+}
+mostPopularApp()
+PCgamesApp()
 // fetch(url)
 // .then(x => x.json())
 // .then(x => console.log(x.results[0].background_image))
