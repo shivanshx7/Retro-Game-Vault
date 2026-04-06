4431cf1b99c01b37cdcd2649a9b1a3d91136a88d
shivanshx7 <shivanshsharma62975@gmail.com>
2026-04-06 23:45:47 +0530

    cleaner css + added action games

--- script.js
+++ script.js
@@ -141,9 +141,31 @@ async function PS5gamesApp() {
 
 }
 
+const ACTIONgames = document.getElementById('action-games')
+
+async function ACTIONgamesApp() {
+    let fetched = await fetch('https://api.rawg.io/api/games?key=713715bfecfd475486ed3f7ef8803ade&platforms=4&genres=action')
+    fetched = await fetched.json()
+    let k = 0
+    fetched.results.reverse()
+
+    for (i of fetched.results){
+        if (k !== 1 ){
+            ACTIONgames.innerHTML += `<div><h3>${i.name}</h3><img src=${i.background_image} ></div>`
+        }
+        k += 1
+    }
+
+
+    console.log(fetched.results)
+
+    
+
+}
 mostPopularApp()
 PCgamesApp()
 PS5gamesApp()
+ACTIONgamesApp()
 // fetch(url)
 // .then(x => x.json())
 // .then(x => console.log(x.results[0].background_image))
